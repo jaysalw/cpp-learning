f18f4fc655d88b3fde262a370d4be0a0f4e69ba8
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-22 20:30:49 +0100

    Enhance vector manipulation by adding pop_back and clear operations with corresponding output statements

--- Vector_Arrays/vector_arrays.cpp
+++ Vector_Arrays/vector_arrays.cpp
@@ -9,5 +9,11 @@ int main() {
     cout << "Vector Size: " << vec.size() << endl;
     cout << "Is empty?: " << vec.empty() << endl;
     cout << "First element: " << vec.at(0) << endl;
+
+    vec.pop_back();
+    cout << "Vector size: " << vec.size() << endl;
+    cout << "Final element: " << vec.back() << endl;
+    vec.clear();
+    cout << "Vector size: " << vec.size() << endl;
     return 0;
 }
