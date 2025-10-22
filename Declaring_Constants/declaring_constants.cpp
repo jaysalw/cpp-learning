fefa108785e33d49c792497fb24198db13bc0d91
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-22 20:44:44 +0100

    Add initial implementation of declaring constants and enums in main function

---
+++ Declaring_Constants/declaring_constants.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const double PI = 3.1415926536;
+    cout << "The value of PI is: " << PI << endl;
+
+    enum
+    {
+        RED=1,
+        YELLOW,
+        GREEN,
+        BROWN,
+        BLUE,
+        PINK,
+        BLACK
+    };
+    cout << "I shot a red worth " << RED << endl;
+    cout << "I shot a blue worth " << BLUE << endl;
+    cout << "Total Scored: " << (RED + BLUE) << endl;
+
+    typedef enum
+    {
+        NEGATIVE, 
+        POSITIVE
+    } charge;
+    charge neutral = NEGATIVE, live = POSITIVE;
+    cout << "Neutral wire: " << neutral << endl;
+    cout << "Live wire: " << live << endl;
+    
+    return 0;
+}
