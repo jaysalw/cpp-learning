905fa65204799d1bf23bb9b18cb9f2baafd705db
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:42:22 +0100

    Refactor main function to include variable initialization and output logic

--- Performing_Operations/Examining_Conditions/examining_conditions.cpp
+++ Performing_Operations/Examining_Conditions/examining_conditions.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() { 
+    int a, b, max;
+    a = 1, b = 2;
+
+    cout << "Variable value is: ";
+    cout << ((a != 1) ? "Not One" : (b == 2) ? "Two" : "One");
+    cout << (( a % 2 != 0) ? " Odd" : " Even");
+    cout << ((a > b) ? "A is greater" : "B is greater");
 
     return 0;
 }
