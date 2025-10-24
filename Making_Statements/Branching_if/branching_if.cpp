830ff36956ef20559be893981e0ae1782d417495
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 20:10:49 +0100

    Implement basic branching logic in branching_if.cpp

--- Making_Statements/Branching_if/branching_if.cpp
+++ Making_Statements/Branching_if/branching_if.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+    const char LETTER = 'A';
+    int num = 8;
     
+    if (num > 5) {
+        cout << "Number is greater than 5" << endl;
+    } else {
+        cout << "Number is less than 5" << endl;
+    }
+
+    if (LETTER == 'A') { 
+        cout << "Letter is A" << endl;
+    }
+
     return 0;
 }
