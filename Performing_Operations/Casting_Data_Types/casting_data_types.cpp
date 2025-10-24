d99e62151aa760822b82beb3e6e59dc2e8aeca19
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 20:04:07 +0100

    Refactor casting_data_types.cpp to include variable initialization and output statements for integer and character casting

--- Performing_Operations/Casting_Data_Types/casting_data_types.cpp
+++ Performing_Operations/Casting_Data_Types/casting_data_types.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    int num = 7,        factor = 2;
+    char letter = 'A';  float result = 0.0;
+
+    cout << "Integer division" << (num / factor) << endl;
+
+    result = (float) num / factor;
+    cout << "Cast devision float: " << result << endl;
+
+    num = static_cast<int>(result);
+    cout << "Cast character int: " << num << endl;
+
+    num = static_cast<int>(letter);
+    cout << "Cast integer char: " << num << endl;
 
     return 0;
 }
