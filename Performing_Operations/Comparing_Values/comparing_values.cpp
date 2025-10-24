3aa9bf89902d28925fe855d3cd1f5a138a7d778b
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:36:12 +0100

    Add inequality and comparison outputs to comparing_values.cpp

--- Performing_Operations/Comparing_Values/comparing_values.cpp
+++ Performing_Operations/Comparing_Values/comparing_values.cpp
@@ -10,5 +10,16 @@ int main() {
     cout << "( == 0) " << (nil == num) << "(true)";
     cout << "(A == a) " << (cap == low) << "(false)";
 
+    cout << endl << "Inequality comparisons:\t";
+    cout << "( != 0) " << (nil != num) << "(false)";
+    cout << endl << "Greater comparisons:\t";
+    cout << "( 1 > 0) " << (max > nil) << "(true)";
+    cout << endl << "Lesser comparisons:\t";
+    cout << "( 0 < 1) " << (nil < max) << "(true)";
+    cout << endl << "Greater or equal comparisons:\t";
+    cout << "( 1 >= 1) " << (max >= max) << "(true)";
+    cout << endl << "Lesser or equal comparisons:\t";
+    cout << "( 0 <= 1) " << (nil <= max) << "(true)";
+
     return 0;
 }
