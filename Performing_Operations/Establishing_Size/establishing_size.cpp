72e2f391d6a1bc815dd8014fa4cca5a0000d8e64
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:51:43 +0100

    Fix output formatting by adding missing newline at the end of the file

--- Performing_Operations/Establishing_Size/establishing_size.cpp
+++ Performing_Operations/Establishing_Size/establishing_size.cpp
@@ -7,5 +7,10 @@ int main() {
     bool isTrue;    unsigned int max;   char letter;
     double pi;      short int number;   char letters[50];
 
+    cout << "int size: " << sizeof(num);
+    cout << "\t50 int size: " << sizeof(nums) << endl;
+    cout << "short int zize: " << sizeof(number) << endl;
+    cout << "unsigned int size: " << sizeof(max) << endl;
+    
     return 0;
 }
