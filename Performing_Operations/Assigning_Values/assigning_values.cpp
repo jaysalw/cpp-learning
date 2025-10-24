22061bce9b18227ea14db9c575ad7ebd5a243c00
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:28:37 +0100

    Implement value assignment and arithmetic operations in C++

--- Performing_Operations/Assigning_Values/assigning_values.cpp
+++ Performing_Operations/Assigning_Values/assigning_values.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main() {
+    int a, b;
+
+    cout << "Assigning Values:" << endl;
+    cout << "a = " << (a = 8) << " b = " << (b = 4);
+
+    cout << endl << "Add & Assign:\t\t" ;
+    cout << endl << "a += b:\t" << (a += b);
+    cout << endl << "Subtract & Assign:\t";
+    cout << endl << "a -= b:\t" << (a -= b);
+    cout << endl << "Multiply & Assign:\t";
+    cout << endl << "a *= b:\t" << (a *= b);
+    cout << endl << "Divide & Assign:\t";
+    cout << endl << "a /= b:\t" << (a /= b);
+    cout << endl << "Modulus & Assign:\t";
+    cout << endl << "a %= b:\t" << (a %= b);
     
+    return 0;
 }
