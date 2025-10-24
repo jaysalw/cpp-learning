7f15abd41bca49e2cd4ae29d8dee8a5f8aae1d50
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:50:14 +0100

    Refactor variable declarations for improved readability and organization

--- Performing_Operations/Establishing_Size/establishing_size.cpp
+++ Performing_Operations/Establishing_Size/establishing_size.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-    
+    int num;        int nums[50];       float decimal;
+    bool isTrue;    unsigned int max;   char letter;
+    double pi;      short int number;   char letters[50];
+
     return 0;
 }
