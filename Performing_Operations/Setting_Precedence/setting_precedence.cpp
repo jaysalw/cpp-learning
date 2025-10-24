7bf3042d2f0e1b79bef9e92e999c2a73ca933d24
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:56:41 +0100

    Add calculations and output statements for default and forced order/direction

--- Performing_Operations/Setting_Precedence/setting_precedence.cpp
+++ Performing_Operations/Setting_Precedence/setting_precedence.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+    int num = 1 + 4 * 3;
+
+    cout << endl << "Default order: " << num;
+
+    num = (1 + 4) * 3;
+    cout << "\tForced order: " << num;
+
+    num = 7 - 4 + 2;
+    cout << endl << "Default direction: " << num;
+
+    num = 7 - ( 4 + 2 );
+    cout << "\tForced direction: " << num;
     
     return 0;
 }
