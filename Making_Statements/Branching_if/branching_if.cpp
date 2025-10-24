2eb5c8eb52ef405b08fbf5758d07c88b00168945
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 20:11:46 +0100

    Add conditional check for num2 in branching_if.cpp

--- Making_Statements/Branching_if/branching_if.cpp
+++ Making_Statements/Branching_if/branching_if.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main() {
     const char LETTER = 'A';
     int num = 8;
+    int num2 = 3;
     
     if (num > 5) {
+        cout << "Number is greater than 5" << endl;
+    } else {
+        cout << "Number is less than 5" << endl;
+    }
+
+    // Where num = 3 instead of 8
+    if (num2 > 5) {
         cout << "Number is greater than 5" << endl;
     } else {
         cout << "Number is less than 5" << endl;
