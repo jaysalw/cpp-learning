15bb5422524d46b4ad7a88c1fa4aa7e63ee8ec3f
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:42:30 +0100

    Fix output message to specify variable 'a' value in examining conditions

--- Performing_Operations/Examining_Conditions/examining_conditions.cpp
+++ Performing_Operations/Examining_Conditions/examining_conditions.cpp
@@ -6,7 +6,7 @@ int main() {
     int a, b, max;
     a = 1, b = 2;
 
-    cout << "Variable value is: ";
+    cout << "Variable a value is: ";
     cout << ((a != 1) ? "Not One" : (b == 2) ? "Two" : "One");
     cout << (( a % 2 != 0) ? " Odd" : " Even");
     cout << ((a > b) ? "A is greater" : "B is greater");
