16ade64360251fc7bad2aef4566301ee71c6dded
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:39:13 +0100

    Implement logical operations output in assessing logic

--- Performing_Operations/Assessing_Logic/asssesing_logic.cpp
+++ Performing_Operations/Assessing_Logic/asssesing_logic.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 int main() {
     int a = 1, b = 1;
+
+    cout << "Logical AND (&&):\t";
+    cout << "( 1 && 1) " << (a && b) << "(true)";
+    cout << endl << "Logical OR (||):\t";
+    cout << "( 1 || 0) " << (a || 0) << "(true)";
+    cout << endl << "Logical NOT (!):\t";
+    cout << "(! 1) " << (!a) << "(false)";
+    
+    return 0;
 }
