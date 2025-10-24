0717bf29222d6293ec092cfdf619b2985c356335
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:34:21 +0100

    Add initial implementation of comparing values in C++

---
+++ Performing_Operations/Comparing_Values/comparing_values.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int nil = 0, num = 0, max = 1; 
+    char cap = 'A', low = 'a';
+
+    cout << "Equality comparisons:\t";
+    cout << "( == 0) " << (nil == num) << "true";
+
+    return 0;
+}
