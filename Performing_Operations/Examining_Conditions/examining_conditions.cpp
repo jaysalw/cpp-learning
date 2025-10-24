d7986fdf551f71a047b1ea684b52b25b2094b546
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-24 19:43:27 +0100

    Enhance output to include variable 'b' value and its properties in examining conditions

--- Performing_Operations/Examining_Conditions/examining_conditions.cpp
+++ Performing_Operations/Examining_Conditions/examining_conditions.cpp
@@ -11,5 +11,12 @@ int main() {
     cout << (( a % 2 != 0) ? " Odd" : " Even");
     cout << ((a > b) ? "A is greater" : "B is greater");
 
+    cout << "Variable b value is: ";
+    cout << ((b != 1) ? "Not One" : (b == 2) ? "Two" : "One");
+    cout << (( b % 2 != 0) ? " Odd" : " Even");
+    cout << ((b > a) ? "B is greater" : "A is greater");    
+
+    max = ( a > b ) ? a : b;
+    cout << endl << "Max value is: " << max;
     return 0;
 }
