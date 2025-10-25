be4647db318c799a5184676625dd90af34473465
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-25 21:02:07 +0100

    Add looping_for.cpp with basic for loop implementation

---
+++ Making_Statements/Looping_For/looping_for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i, j;
+
+    for (j = 1; j < 3; j++) {
+        cout << "Inner loop iteration: " << j << endl;
+    }
+    return 0;
+}
