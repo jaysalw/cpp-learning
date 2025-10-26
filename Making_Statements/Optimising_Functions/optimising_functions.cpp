14c5695d95e509e85114d9c48faea7dd9dd2c21c
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-26 17:30:19 +0000

    Fix return statement in computeFactorials function to ensure proper termination

--- Making_Statements/Optimising_Functions/optimising_functions.cpp
+++ Making_Statements/Optimising_Functions/optimising_functions.cpp
@@ -14,7 +14,7 @@ int computeFactorials(int num, int max) {
     cout << "Factorial of " << num << ": ";
     cout << factorial(num) << endl;
     num++;
-    if(num > max) return -;
+    if(num > max) return 0;
     else return computeFactorials(num, max);
 }
 
