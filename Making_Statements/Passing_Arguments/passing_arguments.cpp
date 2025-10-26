644ceee52c1d2af7d1218e9650fbe93f0601fccd
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-26 17:05:44 +0000

    Refactor temperature conversion logic and enhance user interaction in passing_arguments.cpp

--- Making_Statements/Passing_Arguments/passing_arguments.cpp
+++ Making_Statements/Passing_Arguments/passing_arguments.cpp
@@ -5,5 +5,20 @@ using namespace std;
 float fToC(float degreesF = 32.0);
 
 int main() {
+    float fahrenheit, celsius;
+
+    cout << "Enter a Fahrenheit temperature:\t";
+    cin >> fahrenheit;
+
+    celsius = fToC(fahrenheit);
+
+    cout << fahrenheit << " F is " << celsius << " C";
+
+    cout << endl << "Freezing Point: " << fToC() << " C";
     return 0;
+}
+
+float fToC(float degreesF) {
+    float degreesC = ((5.0/9.0) * (degreesF - 32.0));
+    return degreesC;
 }
