95363016752b797e4c5acd5e97e8e8ee11bd3a8a
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-26 17:27:56 +0000

    Add computeFactorials function to optimising_functions.cpp

--- Making_Statements/Optimising_Functions/optimising_functions.cpp
+++ Making_Statements/Optimising_Functions/optimising_functions.cpp
@@ -8,3 +8,11 @@ int factorial(int);
 int main() {
     return 0;
 }
+
+int comoputeFactorials(int num, int max) {
+    cout << "Factorial of " << num << ": ";
+    cout << factorial(num) << endl;
+    num++;
+    if(num > max) return -;
+    else return computeFactorials(num, max);
+}
