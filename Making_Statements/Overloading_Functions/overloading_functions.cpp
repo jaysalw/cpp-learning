5582381b128cb71c4657c1ab40d8a1f0f8ca1c92
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-26 17:10:23 +0000

    Implement area computation functions for circle and rectangle in overloading_functions.cpp

--- Making_Statements/Overloading_Functions/overloading_functions.cpp
+++ Making_Statements/Overloading_Functions/overloading_functions.cpp
@@ -10,3 +10,12 @@ float computeArea(char, float, float);
 int main() {
     return 0;
 }
+
+float computeArea(float diameter) {
+    float radius = (diameter / 2);
+    return (3.141593 * (radius * radius));
+}
+
+float computeArea(float width, float height) {
+    return(width * height);
+}
