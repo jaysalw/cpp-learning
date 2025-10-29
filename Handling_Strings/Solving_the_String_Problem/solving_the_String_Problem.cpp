944d7ef04e76104ebdd5862f2743255bdbbd9601
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-29 20:43:19 +0000

    Add implementation of string handling in solving the string problem

---
+++ Handling_Strings/Solving_the_String_Problem/solving_the_String_Problem.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string term = "100";
+    int number = 100;
+
+    int num;
+    string text;
+    stringstream stream;
+
+    stream << term;
+    
+    stream >> num;
+
+    num /=4;
+    cout << "Integer value: " << num << endl;
+
+    stream.str("");
+    stream.clear();
+    
+    stream << number;
+    stream >> text;
+    text += " Percent";
+    cout << "String value: " << text << endl;
+}
