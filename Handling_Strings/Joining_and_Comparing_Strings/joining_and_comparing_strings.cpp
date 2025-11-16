707413e0fd90f3a7be2c786768456d9b631f1b38
Jay Salway <2512956@leedstrinity.ac.uk>
2025-11-16 20:51:34 +0000

    Add initial implementation of joining and comparing strings

---
+++ Handling_Strings/Joining_and_Comparing_Strings/joining_and_comparing_strings.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string lang = "C++";
+    string term = "Programming";
+    string text = "C++ Programming";
+
+    return 0;
+}
