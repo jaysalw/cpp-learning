10b6d864821198634c40efddbc1134256b3e7d16
Jay Salway <2512956@leedstrinity.ac.uk>
2025-11-30 23:35:41 +0000

    Implement substring search examples in finding_substrings.cpp

--- Handling_Strings/Finding_Substrings/finding_substrings.cpp
+++ Handling_Strings/Finding_Substrings/finding_substrings.cpp
@@ -7,5 +7,14 @@ int main() {
     string text = "I can resist anything but temptation.";
     int num;
 
+    num = text.find("resist", 0);
+    cout << "\n\"resist\" result: " << num << endl;
+
+    num = text.find("absent", 0);
+    cout << "\"absent\" result: " << num << endl;
+
+    num = text.find_first_of("If");
+    cout << "\nFirst I:\t" << num << endl;
+
     return 0;
 }
