a5b59b018e7b68b3f339431aec053ecd2740417a
Jay Salway <2512956@leedstrinity.ac.uk>
2025-11-30 23:29:41 +0000

    Refactor string assignment and output for clarity in copying and swapping example

--- Handling_Strings/Copying_and_Swapping_Strings/copying_and_swapping_strings.cpp
+++ Handling_Strings/Copying_and_Swapping_Strings/copying_and_swapping_strings.cpp
@@ -7,5 +7,18 @@ int main() {
     string front;
     string back;
     string text = "Always laugh when you can, it\'s free medicine";
+
+    front.assign(text);
+
+    cout << endl << "Front: " << front << endl;
+
+    front.assign(text, 0, 27);
+
+    cout << endl << "Front: " << front << endl;
+
+    back.assign(text, 27, text.size());
+
+    cout << "Back: " << back << endl;
+
     return 0;
 }
