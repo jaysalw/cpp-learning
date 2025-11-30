421308cbc082695de5d4b1f7c146e63d30f0cf51
Jay Salway <2512956@leedstrinity.ac.uk>
2025-11-30 23:27:47 +0000

    Add basic structure for copying and swapping strings example

---
+++ Handling_Strings/Copying_and_Swapping_Strings/copying_and_swapping_strings.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string front;
+    string back;
+    string text = "Always laugh when you can, it\'s free medicine";
+    return 0;
+}
